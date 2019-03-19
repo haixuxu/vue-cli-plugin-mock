77cb3e44006f2052fbcb0e986267620ae0f06db8
xuxihai <xuxihai123@gmail.com>
2019-03-20 00:54:04 +0800

    support vue add plugin generator mock template

---
+++ generator/template/mock/index.js
@@ -0,0 +1,49 @@
+export default {
+    'GET /api/user': {
+        username: 'admin',
+        sex: 5,
+    },
+    'GET /api/list': function (req, res) {
+        let query = req.query || {};
+        return res.json({
+            limit: query.limit,
+            offset: query.offset,
+            list: [{
+                username: 'admin1',
+                sex: 1,
+            }, {
+                username: 'admin2',
+                sex: 0,
+            }]
+        })
+    },
+    'GET /api/userinfo/:id': (req, res) => {
+        return res.json({
+            id: req.params.id,
+            username: 'kenny',
+        });
+    },
+    'POST /api/login/account': (req, res) => {
+        const { password, username } = req.body;
+        if (password === '888888' && username === 'admin') {
+            return res.json({
+                status: 'ok',
+                token: 'sdfsdfsdfdsf',
+            });
+        } else {
+            return res.json({
+                status: 'error',
+                code: 403,
+            });
+        }
+    },
+    'DELETE /api/user/:id': (req, res) => {
+        // console.log(req.params.id);
+        res.send({ status: 'ok', message: '删除成功！' });
+    },
+    'PUT /api/user/:id': (req, res) => {
+        // console.log(req.params.id);
+        // console.log(req.body);
+        res.send({ status: 'ok', message: '修改成功！' });
+    },
+}
