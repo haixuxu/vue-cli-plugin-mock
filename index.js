9f98c69cce60fea5cfd900b9d1d5c5b8823997ec
xuxihai <xuxihai123@gmail.com>
2019-04-09 19:47:43 +0800

    support disable all mock

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const MockMiddleware = require('./mock');
 const webpackWatch = require('./mock/webpackWatch');
-
+const logger = require('./mock/logger');
 module.exports = (api, options) => {
   if (process.env.NODE_ENV === 'development') {
     const webpack = require(api.resolve('node_modules/webpack'));
     webpackWatch.configWebpack(webpack);
     const mockOptions = options.pluginOptions && options.pluginOptions.mock || {};
+    if (mockOptions.disable) {
+      logger('mock middleware disabled!');
+      return;
+    }
     let entry = mockOptions.entry || './mock/index.js';
     mockOptions.entry = api.resolve(entry);
     api.configureDevServer(function (app, server) {
